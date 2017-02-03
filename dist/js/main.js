3010c2cf4bc9934dbe26a4dcef7c1bbf2fc2d8b0
Mykola <mykola.shevchuk.ua@gmail.com>
2017-02-03 06:18:42 +0200

    without pp

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,41 @@
-jQuery(document).ready(function($) {
-	$('h1').on('click', function() {
-		$(this).css({
-			"color": 'red',
-			"text-decoration": 'line-through'
+$(document).ready(function($) {
+
+	/*MENU HOVERS*/
+
+	var $headerLinks = $('.header-menu a'),
+		$menuCatalogLinks = $('.menu-catalog a');
+
+	function sortLinks(variable) {
+		variable.on('click', function(event) {
+			event.preventDefault();
+
+			variable.each(function(index, el) {
+				$(el).removeClass('active')
+			});
+
+			$(this).addClass('active');
 		});
-	});
+	} 
+
+	sortLinks($headerLinks);
+	sortLinks($menuCatalogLinks);
+
+	/*CURRENCY AND LANUAGES NAV BAR*/
+
+	var $currencyPanel = $('.currency-panel'),
+		$currency = $currencyPanel.find('.currency'),
+		$languagesPanel = $('.languages-panel'),
+		$languages = $languagesPanel.find('.languages');
+
+		$currencyPanel.on('click', function() {
+			$currencyPanel.find('ul').toggleClass('down-arrow');
+			$currency.slideToggle();
+		});
+
+		$languagesPanel.on('click', function() {
+			$languagesPanel.find('ul').toggleClass('down-arrow');
+			$languages.slideToggle();
+		});
+
 });
 	
